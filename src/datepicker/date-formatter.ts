valor-software/ngx-bootstrap

commit 27a022922370947d54c3cfd357399eb588e8d01f

feat(package): changed the way moment is imported (#1556)
* Fix rollup error.

Rollup cannot process `import * as moment from 'moment'`, instead `import moment from 'moment'` should work.
See https://github.com/rollup/rollup/issues/670

* fix typescript compilation

src/datepicker/date-formatter.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 
 export class DateFormatter {
   public format(date:Date, format:string):string {

murakamiharuto/Bootstrap.

commit 650403d0cfdc6917b5699cc53f0c78123353f4b8

fix(datepicker): refix issue with moment import (#2023)
- this was issue , broken by 27a022922370947d54c3cfd357399eb588e8d01f
fixes #1556

src/datepicker/date-formatter.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 
 export class DateFormatter {
   public format(date:Date, format:string):string {